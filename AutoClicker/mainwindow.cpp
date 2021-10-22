c533d3d4adcc05518250548c7f355cc6808d1f47
BOB450 <bwvoskuil@gmail.com>
2021-10-22 13:31:50 -0600

    Update mainwindow.cpp

--- AutoClicker/mainwindow.cpp
+++ AutoClicker/mainwindow.cpp
@@ -34,4 +34,25 @@ void MainWindow::pushButton()
     QMessageBox msgBox;
     msgBox.setText("Set click intervel to: " +  textvalue);
     msgBox.exec();
+
+
+    bool click = false; //sets click to false
+
+    while (true)
+    {
+        if (GetAsyncKeyState('X')) //if X is pressed click = true
+        {
+            click = true;
+        }
+        else if (GetAsyncKeyState('Z')) //if 'Z' is pressed click = false
+        {
+            click = false;
+        }
+        if (click == true) // if click = true it will press the mouse button down and up really fast
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Sleep(b);
+        }
+    }
 }
