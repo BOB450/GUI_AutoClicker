46cc306672446787a49ede764c01d83e22e8b35b
BOB450 <bwvoskuil@gmail.com>
2021-10-22 13:25:43 -0600

    the ui now has a button and input box
    added button to slotts

--- AutoClicker/mainwindow.cpp
+++ AutoClicker/mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <string>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QLineEdit>
+#include <iostream>
+#include <Windows.h>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::pressed, this, &MainWindow::pushButton);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +21,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::pushButton()
+{
+
+  QString  textvalue = ui->lineEdit->displayText();
+
+
+
+    double b = textvalue.toDouble();
+
+
+    QMessageBox msgBox;
+    msgBox.setText("Set click intervel to: " +  textvalue);
+    msgBox.exec();
+}
